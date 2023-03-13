b937537a74364261b4c1481e32932477446962ba
ARTEM1349 <64367956+ARTEM1349@users.noreply.github.com>
2023-03-13 12:04:11 +0200

    Update main.c

--- module-main/src/main.c
+++ module-main/src/main.c
@@ -1,61 +1,45 @@
-#include "lib.h"
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #include <malloc.h>
-#include <check.h>
+#include "lib.h"
 
-START_TEST(test_lab05)
+int main(int argc, char *argv[]) 
 {
-        int test_a = 1;
-        int test_n = 1;
-        int test_i = 200;
-        int test_result = 2000;
-        int actual_result = lab05(test_a,test_n);
-  
-        ck_assert_int_eq(test_result, actual_result);
-				
-}
-END_TEST
-
-START_TEST(test_lab06)
-{
-        int i ,j,c;
-        int s = 6;
-       long int expected[5]= {24,342,345,234,545,678};
-         int actual = lab06(i,j,c); 
-         ck_assert_int_eq(actual, expected[5]);
-}
-END_TEST
-Suite *lab_test_suite(void)
-{
-    Suite *s;
-    TCase *tc_core;
+    int a, i,b;
+    long int  d[]= {24,342,345,234,545,678};
+    long int  *t;
+    unsigned int s = 6; 
+    char *c;
     
-    s = suite_create("lab09");
-    tc_core = tcase_create("Core");
+    srand((unsigned int)time(NULL));
+    a = rand() % 10;
+    lab05(a);
     
-    tcase_add_test(tc_core, test_lab05);
-    tcase_add_test(tc_core, test_lab06);
-    suite_add_tcase(s, tc_core);
+    if(argc > 1)
+    {
+      s = 0;
+      for(i = 1; i < argc; i++)
+      {
+      	s++;
+      }
+      
+      
+      t = (long int*) malloc (s * sizeof(long int));
+      for(i = 1, b = 0; i < argc; i++, b++)
+      {
+      	 t[b] = strtol(argv[i], &c, 10);
+      }
+            
+      lab06(t,s);
+      
+    }
+    else
+    {  
+      lab06(d,s);     
+    }
     
-    return s;
-}
-
-int main(void)
-{
-    
-    int number_failed;
-    Suite *s;
-    SRunner *sr;
-    
-    s = lab_test_suite();
-    sr = srunner_create(s);
-    
-    srunner_run_all(sr, CK_NORMAL);
-    number_failed = srunner_ntests_failed(sr);
-    srunner_free(sr);
-    
-    return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
-
+    return 0;
 }
